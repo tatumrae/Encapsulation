01e6874e0f2947da8481f298c1b7b468ffe1b914
Tatum Thomas <tthomas31@my.wctc.edu>
2017-01-31 21:45:49 -0600

    Completed method and property encapsulation, created helper methods

--- src/lab2/Employee.java
+++ src/lab2/Employee.java
@@ -35,36 +35,43 @@ public class Employee {
         this.lastName = lastName;
         this.ssn = ssn;
     }
-
-    // Assume this must be performed first, and assume that an employee
-    // would only do this once, upon being hired.
-    public void meetWithHrForBenefitAndSalryInfo() {
-        metWithHr = true;
+    
+    private String getFormattedDate() {
         SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
-        String fmtDate = sdf.format(orientationDate);        
-        System.out.println(firstName + " " + lastName + " met with Hr on "
-            + fmtDate);
+        String fmtDate = sdf.format(orientationDate); 
+        return fmtDate;
+    }
+    
+    public void completeOrientationAfterBeingHired() {
+        meetWithHrForBenefitAndSalaryInfo();
+        meetDepartmentStaff();
+        reviewDeptPolicies();
+        moveIntoCubicle(cubeId);
     }
 
     // Assume this must be performed first, and assume that an employee
+    // would only do this once, upon being hired.
+    private void meetWithHrForBenefitAndSalaryInfo() {
+        metWithHr = true;     
+        System.out.println(firstName + " " + lastName + " met with Hr on "
+            + getFormattedDate());
+    }
+
+    // Assume this must be performed second, and assume that an employee
     // would only do this once, upon being hired.:
-    public void meetDepartmentStaff() {
-        metDeptStaff = true;
-        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
-        String fmtDate = sdf.format(orientationDate);        
+    private void meetDepartmentStaff() {
+        metDeptStaff = true;         
         System.out.println(firstName + " " + lastName + " met with Dept. Staff on "
-            + fmtDate);
+            + getFormattedDate());
     }
 
     // Assume this must be performed third. And assume that because department
     // policies may change that this method may need to be called 
     // independently from other classes.
     public void reviewDeptPolicies() {
-        reviewedDeptPolicies = true;
-        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
-        String fmtDate = sdf.format(orientationDate);        
+        reviewedDeptPolicies = true;  
         System.out.println(firstName + " " + lastName + " reviewed Dept policies on "
-            + fmtDate);
+            + getFormattedDate());
     }
 
     // Assume this must be performed 4th. And assume that because employees
@@ -72,11 +79,9 @@ public class Employee {
     // independently from other classes.
     public void moveIntoCubicle(String cubeId) {
         this.cubeId = cubeId;
-        this.movedIn = true;
-        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
-        String fmtDate = sdf.format(orientationDate);        
+        this.movedIn = true; 
         System.out.println(firstName + " " + lastName + " moved into cubicle "
-                + cubeId + " on " + fmtDate);
+            + cubeId + " on " + getFormattedDate());
     }
 
     public String getFirstName() {
